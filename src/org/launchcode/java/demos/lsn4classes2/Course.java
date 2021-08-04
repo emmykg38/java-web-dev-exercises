4534e3c14eef89855865a1002ab07fe749e84f6e
millylane003 <emilykgerst19@gmail.com>
2021-08-04 17:30:18 -0500

    finished exercisese

--- src/org/launchcode/java/demos/lsn4classes2/Course.java
+++ src/org/launchcode/java/demos/lsn4classes2/Course.java
@@ -1,17 +1,56 @@
 package org.launchcode.java.demos.lsn4classes2;
 
+
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Course {
     private String topic;
     private Teacher instructor;
     private ArrayList<Student> enrolledStudents;
 
+    public Course(String topic, Teacher instructor, ArrayList<Student> enrolledStudents) {
+        this.topic = topic;
+        this.instructor = instructor;
+        this.enrolledStudents = enrolledStudents;
+    }
+
+
+
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "topic='" + topic + '\'' +
+                ", instructor=" + instructor +
+                ", enrolledStudents=" + enrolledStudents +
+                '}';
+    }
 
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(topic, course.topic) && Objects.equals(instructor, course.instructor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(topic, instructor);
+    }
+
+
+
 }
+
+
